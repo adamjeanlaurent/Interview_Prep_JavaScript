2ff6ea193ab831f443f0c387d3f739725b76f76b
Adam Jean-Laurent <ajean-laurent@my.uri.edu>
2019-08-25 15:29:00 -0400

    almost done with delete function

--- Trees/recBST.js
+++ Trees/recBST.js
@@ -6,6 +6,8 @@ class BSTNode {
     }
 }
 
+// TODO comment these functions so that you understand them
+
 function insert(root, data) {
     if(root == null) {
         root = new BSTNode(data);
@@ -22,11 +24,69 @@ function insert(root, data) {
     return root;
 }
 
-function search(root, data)
+function inorder(node){
+    if(node){
+       inorder(node.left);
+       console.log(node.data);
+       inorder(node.right);
+    }
+ }
+
+function search(root, data) {
+    if(root == null) return false;
+    else if(root.data == data) return true;
+    if(data => root.data) return search(root.right, data);
+    else return search(root.left, data);
+}
+
+function deleteNode(root, data) {
+    if(root == null) return root;
+    else if(data < root.data) root.left = deleteNode(root.left, data);
+    else if(data > root.data) root.right = deleteNode(root.right, data);
+    
+    // node to delete found
+    else {
+        // CASE 1: Node to delete has no children
+        if(root.left == null && root.right == null) {
+            root = null;
+            return root;
+        }
+        // CASE 2: Node to delete has one child
+        else if(root.right == null) {
+            /*
+            - move a ptr to the child
+            - delete the root
+            - return temp
+            */
+
+            let temp = root.left;
+            root = null;
+            return temp;
+        }
+
+        else if (root.left == null) {
+            let temp = root.right;
+            root = null;
+            return temp;
+        }
+
+        else {
+            let successor = findMin(root.right);
+            root.data = successor.data;
+            root.right = deleteNode(root.right, successor.data);
+            return root;
+        }
+    }
+    return root;
+}
 
 let root = null;
 root = insert(root,10);
 root = insert(root,20);
 root = insert(root,30);
 root = insert(root,40);
-// console.log(root);
+root = insert(root,38);
+root = insert(root,42);
+
+root = deleteNode(root, 40);
+inorder(root);
