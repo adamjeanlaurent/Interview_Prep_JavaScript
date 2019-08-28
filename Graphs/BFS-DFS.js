784295d7a520856a9c176439fd03d753aad3e636
Adam Jean-Laurent <ajean-laurent@my.uri.edu>
2019-08-28 19:34:23 -0400

    depth-first search

--- Graphs/BFS-DFS.js
+++ Graphs/BFS-DFS.js
@@ -16,9 +16,26 @@ function bfs(start) {
                 queue.push(adjacent);
             }
         });
-        }
     }
+}
 
 function dfs(start) {
     let stack = [];
+    let seen = new Set();
+
+    stack.push(start);
+
+    while(stack.length != 0) {
+        let cur = stack.pop();
+        if(!seen.has(cur)) {
+            seen.add(cur);
+            console.log(cur);
+        }
+        cur.adjacents.forEach((adj) => { // if adjacents is an array of adjacent elements
+            if(!seen.has(adjacent)) {
+                stack.push(adjacent);
+            }
+        });
+
+    }
 }
